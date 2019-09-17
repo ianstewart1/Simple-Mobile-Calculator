2d5f78166551b4962059680599206007e4673578
ianstewart1 <stewarim@whitman.edu>
2019-09-17 23:36:40 +0200

    Added multiply and divide

--- app/src/main/java/us/istewart/aithello/MainActivity.kt
+++ app/src/main/java/us/istewart/aithello/MainActivity.kt
@@ -1,10 +1,17 @@
 package us.istewart.aithello
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.inputmethod.InputMethodManager
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,20 +19,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnAdd.setOnClickListener{
-            var num1 = etName1.text.toString()
-            var num2 = etName2.text.toString()
+        fun onButtonPress() :Pair<Double, Double> {
+            try {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            } catch (e: Exception) {
+                // TODO: handle exception
+            }
 
-            var result = Integer.parseInt(num1) + Integer.parseInt(num2)
-            tvData.text = "Result: %d".format(result)
+            var num1 = etName1.text.toString().toDouble()
+            var num2 = etName2.text.toString().toDouble()
+            // TODO: check for input validity!!!
+            // TODO: clear text entry boxes!!!
+            // TODO: add labeling for operand boxes
+            
+            return Pair(num1, num2)
+        }
+
+        btnAdd.setOnClickListener{
+
+            var (num1, num2) = onButtonPress()
+
+            var result = num1 + num2
+
+            tvData.text = "%.2f + %.2f = %.2f".format(num1, num2, result)
         }
 
         btnMinus.setOnClickListener{
-            var num1 = etName1.text.toString()
-            var num2 = etName2.text.toString()
+            var (num1, num2) = onButtonPress()
 
-            var result = Integer.parseInt(num1) - Integer.parseInt(num2)
-            tvData.text = "Result: %d".format(result)
+            var result = num1 - num2
+
+            tvData.text = "%.2f - %.2f = %.2f".format(num1, num2, result)
+        }
+
+        btnMult.setOnClickListener{
+            var (num1, num2) = onButtonPress()
+
+            var result = num1 * num2
+
+            tvData.text = "%.2f * %.2f = %.2f".format(num1, num2, result)
+        }
+
+        btnDiv.setOnClickListener{
+            var (num1, num2) = onButtonPress()
+
+            var result = num1 / num2
+
+            tvData.text = "%.2f / %.2f = %.2f".format(num1, num2, result)
         }
     }
 }
